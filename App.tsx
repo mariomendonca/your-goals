628e805a3852def5e8f9df8166d7e3d1d7cf79d1
mario mendonca <mariomeendonca00@gmail.com>
2022-09-27 21:38:02 -0300

    feat: splash screen await fonts loading

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
+import { useCallback, useEffect } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
 import {
   Nunito_400Regular,
   Nunito_700Bold,
   Nunito_500Medium,
   useFonts
 } from '@expo-google-fonts/nunito'
+
 import { Home } from './src/Home';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   const [isFontsLoaded] = useFonts({
@@ -12,6 +17,21 @@ export default function App() {
     Nunito_500Medium,
     Nunito_700Bold
   })
+
+  const hideSplash = useCallback(async () => {
+    await SplashScreen.hideAsync()
+  }, [isFontsLoaded])
+
+  useEffect(() => {
+    if (!isFontsLoaded) return
+
+    hideSplash()
+  }, [isFontsLoaded])
+
+
+  if (!isFontsLoaded) {
+    return null
+  }
 
   return (
     <Home />
